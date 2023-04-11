da550fc5d5abc1119812ad86f9c521f57357d9ad
tigerkzr <tigerkzr@gmail.com>
2023-04-11 18:50:21 -0400

    more scratch

--- source/reorder_audio_streams_by_channel_count/plugin.py
+++ source/reorder_audio_streams_by_channel_count/plugin.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+"""
+    Written by:               TigerKZR <tigerkzr@adhdtiger.net>
+    Date:                     11 April 2023, (20:00)
+
+    Copyright:
+        Copyright (C) 2023 Michael Kaeser
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 import logging
 
 from unmanic.libs.unplugins.settings import PluginSettings
+
+from reorder_audio_streams_by_channel_count.lib.ffmpeg import StreamMapper, Probe, Parser
 
 # Configure plugin logger
 logger = logging.getLogger("Unmanic.Plugin.reorder_audio_streams_by_channel_count")
